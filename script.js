2b79eca0e64c4aa046109fab89fd1927bf771dc9
Philip Haggard <philip.r.haggard@gmail.com>
2024-07-18 09:21:42 -0500

    Implement human choice function

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ let getComputerChoice = () => {
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random()*choices.length)];
 };
+
+let getHumanChoice = () => {
+    let humanChoice = prompt("Do you choose rock, paper, or scissors? Please enter your choice below:").toLowerCase();
+    while(!(humanChoice === 'rock' || humanChoice === 'paper' || humanChoice === 'scissors')) {
+        humanChoice = prompt("You did not enter a valid choice. Please enter rock, paper, or scissors below:").toLowerCase();
+    }
+    return humanChoice;
+};
